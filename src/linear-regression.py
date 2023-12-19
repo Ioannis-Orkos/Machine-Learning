5a057dc011a451fe274dc31aac3ad9e20252b462
Ioannis Orkos <yohannisnicos@gmail.com>
2023-12-19 16:15:48 +0000

    Refactor linear regression script into functions
    Refactored the linear regression example script to improve readability
    and modularity. The random data generation, model training, and plotting
    code have been encapsulated into separate functions. This change
    enhances code organization, allows for easier maintenance, and improves
    the reusability of individual components.

--- src/linear-regression.py
+++ src/linear-regression.py
@@ -2,20 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# Generate random data
-np.random.seed(0)
-X = 2 * np.random.rand(100, 1)
-y = 4 + 3 * X + np.random.randn(100, 1)
+# Generate some random data
+def generate_data():
+    np.random.seed(0)
+    X = 2 * np.random.rand(100, 1)
+    y = 4 + 3 * X + np.random.randn(100, 1)
+    return X, y
 
-# Create a Linear Regression model
-model = LinearRegression()
-model.fit(X, y)
 
-# Plotting the data and the model's predictions
-plt.scatter(X, y, color='blue', label='Data points')
-plt.plot(X, model.predict(X), color='red', label='Linear regression line')
-plt.xlabel('X (independent variable)')
-plt.ylabel('y (dependent variable)')
-plt.title('Simple Linear Regression Example')
-plt.legend()
-plt.show()
+# Train a linear regression model on the data
+def train_linear_regression(X, y):
+    model = LinearRegression()
+    model.fit(X, y)
+    return model
+
+
+# Plot the data and the linear regression line
+def plot_regression(X, y, model):
+    plt.scatter(X, y, color='blue', label='Data points')
+    plt.plot(X, model.predict(X), color='red', label='Linear regression line')
+    plt.xlabel('X (independent variable)')
+    plt.ylabel('y (dependent variable)')
+    plt.title('Simple Linear Regression Example')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    X, y = generate_data()
+    model = train_linear_regression(X, y)
+    plot_regression(X, y, model)
